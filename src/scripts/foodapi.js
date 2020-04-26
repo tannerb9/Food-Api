c12a9302c4c4d212a616765cfc1eb371a3c9c740
tannerb9 <tanner-brainard@hotmail.com>
2020-04-26 12:29:27 -0500

    added fetch within fetch to access data via barcode

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,22 +1,65 @@
-// Create a function which returns a string template. The template is the
-// HTML representation for a food item.
-// Create a function that inserts an HTML representation of a food into the DOM
+// List the following additional information.
+
+// Calories per serving
+// Fat per serving
+// Sugar per serving
+
 fetch("http://localhost:8088/food")
-  .then((foods) => foods.json())
-  .then((parsedFoods) => {
-    parsedFoods.forEach((food) => {
-      const foodAsHTML = foodFactory(food);
-      addFoodToDom(foodAsHTML);
+  .then((response) => response.json())
+  .then((myParsedFoods) => {
+    myParsedFoods.forEach((food) => {
+      fetch(
+        `https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`
+      )
+        .then((response) => response.json())
+        .then((productInfo) => {
+          // debugger;
+          if (productInfo.product.ingredients_text) {
+            food.ingredients = productInfo.product.ingredients_text;
+          } else {
+            food.ingredients = "No ingredients listed";
+          }
+          if (productInfo.product.countries) {
+            food.country = productInfo.product.countries;
+          } else {
+            food.country = "No countries listed";
+          }
+          if (productInfo.product["nutriment_energy-kcal-tr"]) {
+            food.kcal = productInfo.product["nutriment_energy-kcal-tr"];
+          } else {
+            food.kcal = "Content not available";
+          }
+          if (productInfo.product.nutriment_label["fat"]) {
+            food.fat = productInfo.product.nutriment_label["fat"];
+          } else {
+            food.fat = "Content not available";
+          }
+          if (productInfo.product.sugar) {
+            food.sugar = productInfo.product.sugar;
+          } else {
+            food.sugar = "Content not available";
+          }
+
+          // Produce HTML representation
+          const foodAsHTML = foodFactory(food);
+
+          // Add representaiton to DOM
+          addFoodToDom(foodAsHTML);
+        });
     });
   });
 
-// debugger;
 function foodFactory(thing) {
   return `
   <div class="children">
     <h1>${thing.name}</h1>
-    <p>${thing.category}</p>
-    <p>${thing.ethnicity}</p>
+    <p>Origin: ${thing.origin}</p>
+    <p>Category: ${thing.category}</p>
+    <p>Ingredients: ${thing.ingredients}</p>
+    <p>Calories: ${thing.kcal}</p>
+    <p>Fats: ${thing.fat}</p>
+    <p>Sugars: ${thing.sugar}</p>
+    <p>Available in: ${thing.country}</p>
   </div>
   `;
 }
